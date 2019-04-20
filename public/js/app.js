3d8af4d059f5066db3c1da502504f06c7af197e7
Arun Mishra <arunmishra152@gmail.com>
2019-04-21 04:50:00 +0530

    removing unneccesary sapce and console

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-console.log('Ok, client side console is working fine now');
-
-// fetch('http://localhost:3000/weather?address=boston').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data);
-//     })
-// })
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#para1');
